5bc7b3919bcc877c552434abf43e9f331e62f053
yasshi2525 <yasshy2@gmail.com>
2025-12-17 06:22:10 +0900

    misc: PlaylogClientLike の露出を抑制

--- playlogClient-like/src/createPlaylogClient.ts
+++ playlogClient-like/src/createPlaylogClient.ts
@@ -1,3 +1,5 @@
+import { PlaylogClient } from "./PlaylogClient";
 import { PlaylogClientLike } from "./PlaylogClientLike";
 
-export const createPlaylogClient = () => new PlaylogClientLike();
+export const createPlaylogClient = () =>
+    new PlaylogClientLike() as PlaylogClient;
