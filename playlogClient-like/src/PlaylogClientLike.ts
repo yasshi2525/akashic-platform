8ec51f2def0460a288bf3405d967d8012b3d2193
yasshi2525 <yasshy2@gmail.com>
2025-12-16 21:56:40 +0900

    misc: 不要な戻り値の型指定を削除

--- playlogClient-like/src/PlaylogClientLike.ts
+++ playlogClient-like/src/PlaylogClientLike.ts
@@ -1,12 +1,11 @@
 import { ProtocolType } from "./akashic-gameview";
 import { PlaylogClient } from "./PlaylogClient";
-import { Session } from "./Session";
 import { SessionLike } from "./SessionLike";
 import { SessionOptions } from "./parameters";
 
 export class PlaylogClientLike implements PlaylogClient {
     Socket = { Type: { ...ProtocolType } };
-    Session(url: string, opts: SessionOptions): Session {
+    Session(url: string, opts: SessionOptions) {
         return new SessionLike(url, opts);
     }
 }
