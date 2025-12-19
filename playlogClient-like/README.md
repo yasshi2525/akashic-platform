6f6ef4b3cabec910a630a0de68e66f4a5a01e546
yasshi2525 <yasshy2@gmail.com>
2025-12-19 20:38:32 +0900

    fix: playlogClientV0.1.0 ファイルでモジュール解決できない問題を修正

--- playlogClient-like/README.md
+++ playlogClient-like/README.md
@@ -1,3 +1,8 @@
 # About
 
 [`@akashic/agvw`](https://github.com/akashic-games/agvw) の実行時に必要となる `playlogClinet` (ソース非公開) の独自実装です。
+
+## NOTE
+
+ディストリビューションファイルには `socket.io-client` もバンドルされています。
+これは本ライブラリ利用するクライアント側で別途モジュール解決させることができなかったための措置です。
--- playlogClient-like/tsup.config.ts
+++ playlogClient-like/tsup.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "tsup";
+import * as path from "node:path";
+import process from "node:process";
+import { createRequire } from "node:module";
+
+const require = createRequire(path.join(process.cwd(), path.sep));
+
+const globalName =
+    "playlogClientV" + require("./package.json").version.replace(/[\.-]/g, "_");
 
 export default defineConfig({
     entry: ["src/index.ts"],
+    globalName,
     target: "es6",
     format: "iife",
     platform: "browser",
