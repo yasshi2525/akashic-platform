f5edaf5a2252553acb16c21ce9ebf11893c4160d
yasshi2525 <yasshy2@gmail.com>
2025-12-22 17:54:38 +0900

    fix: AMFlowサーバー側で権限確認してない

--- akashic-storage/src/initializeSocket.ts
+++ akashic-storage/src/initializeSocket.ts
@@ -1,4 +1,5 @@
 import type { Socket } from "socket.io";
+import type { Permission } from "@akashic/amflow";
 import {
     ListenSchema,
     EmitSchema,
@@ -12,6 +13,7 @@ export const initializeSocket = (
     amfManager: AMFlowServerManager,
 ) => {
     let server: AMFlowServer | null = null;
+    let permission: Permission | null = null;
     const assertsOpen = () => {
         if (server == null) {
             throw new Error("this session isn't opened.");
@@ -44,7 +46,7 @@ export const initializeSocket = (
     socket.on(ListenEvent.Authenticate, async (token, cb) => {
         try {
             assertsOpen();
-            const permission = await server!.authenticate(token);
+            permission = await server!.authenticate(token);
             cb(null, permission);
         } catch (err) {
             cb((err as Error).message, undefined);
@@ -53,18 +55,27 @@ export const initializeSocket = (
     socket.on(ListenEvent.SendTick, async (tick) => {
         try {
             assertsOpen();
+            if (!permission?.writeTick) {
+                return;
+            }
             await server!.sendTick(tick);
         } catch (err) {}
     });
     socket.on(ListenEvent.SendEvent, (event) => {
         try {
             assertsOpen();
+            if (!permission?.sendEvent) {
+                return;
+            }
             server!.sendEvent(event);
         } catch (err) {}
     });
     socket.on(ListenEvent.SubscribeTick, () => {
         try {
             assertsOpen();
+            if (!permission?.subscribeTick) {
+                return;
+            }
             server!.subscribeTick(socket);
         } catch (err) {}
     });
@@ -77,6 +88,9 @@ export const initializeSocket = (
     socket.on(ListenEvent.SubscribeEvent, () => {
         try {
             assertsOpen();
+            if (!permission?.subscribeEvent) {
+                return;
+            }
             server!.subscribeEvent(socket);
         } catch (err) {}
     });
@@ -89,6 +103,9 @@ export const initializeSocket = (
     socket.on(ListenEvent.GetTickList, async (opts, cb) => {
         try {
             assertsOpen();
+            if (!permission?.readTick) {
+                return;
+            }
             const tickList = await server!.getTickList(opts);
             cb(null, tickList); // NOTE: tickList が null なのは正常
         } catch (err) {
@@ -98,6 +115,9 @@ export const initializeSocket = (
     socket.on(ListenEvent.GetStartPoint, async (opts, cb) => {
         try {
             assertsOpen();
+            if (!permission?.readTick) {
+                cb("permission error", undefined);
+            }
             const startPoint = await server!.getStartPoint(opts);
             if (startPoint) {
                 cb(null, startPoint);
@@ -111,6 +131,9 @@ export const initializeSocket = (
     socket.on(ListenEvent.PutStartPoint, async (startPoint, cb) => {
         try {
             assertsOpen();
+            if (!permission?.writeTick) {
+                cb("permission error");
+            }
             await server!.putStartPoint(startPoint);
             cb(null);
         } catch (err) {
