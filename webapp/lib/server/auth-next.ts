3a823f387d465664b2c627defd640a1109ce5c7d
yasshi2525 <yasshy2@gmail.com>
2025-12-26 23:34:44 +0900

    impl: add prisma

--- webapp/lib/server/auth-next.ts
+++ webapp/lib/server/auth-next.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [GitHub],
     callbacks: {
         session({ session }) {
