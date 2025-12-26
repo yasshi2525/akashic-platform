8eb94177268ef44bd01e3aa46e4e2a77d7f33ef8
yasshi2525 <yasshy2@gmail.com>
2025-12-26 23:44:56 +0900

    misc: 不要な name 補完を削除 DBが自動で採番してくれるので

--- webapp/lib/server/auth-next.ts
+++ webapp/lib/server/auth-next.ts
@@ -6,12 +6,4 @@ import { prisma } from "./prisma";
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [GitHub],
-    callbacks: {
-        session({ session }) {
-            if (!session.user.id && session.user.name) {
-                session.user.id = session.user.name;
-            }
-            return session;
-        },
-    },
 });
