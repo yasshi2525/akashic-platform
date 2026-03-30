ec442dae91c20e57e7140fdc2c4e90f158934fae
yasshi2525 <yasshy2@gmail.com>
2026-03-30 16:01:02 +0900

    improve: クライアントエラーをより捕捉できるように GameDriver レベルでも捕捉

--- webapp/lib/client/akashic-container.ts
+++ webapp/lib/client/akashic-container.ts
@@ -133,15 +133,16 @@ export class AkashicContainer {
                 param.onSkip(isSkipping);
             },
         });
+        const handleError = (err: unknown) => {
+            param.onError(
+                "予期しないエラーが発生したため、ゲームを停止しました。ゲームの再開を試みる場合は画面を更新してください。",
+            );
+            console.error(err);
+            content.pause();
+            param.onOpenTroubleshoot();
+        };
         content.addErrorListener({
-            onError: (err) => {
-                param.onError(
-                    "予期しないエラーが発生したため、ゲームを停止しました。ゲームの再開を試みる場合は画面を更新してください。",
-                );
-                console.error(err);
-                content.pause();
-                param.onOpenTroubleshoot();
-            },
+            onError: handleError,
         });
         content.addContentLoadListener({
             onLoad: () => {
@@ -194,8 +195,9 @@ export class AkashicContainer {
                     c.warn = makeOverride("warn", c.warn.bind(c));
                     c.error = makeOverride("error", c.error.bind(c));
                 }
-                const amflowcontent = content.getGameDriver()!._platform
-                    .amflow as AMFlowClient;
+                const driver = content.getGameDriver()!;
+                driver.errorTrigger.add(handleError);
+                const amflowcontent = driver._platform.amflow as AMFlowClient;
                 amflowcontent.onPlayEnd((reason) => param.onPlayEnd(reason));
                 amflowcontent.onPlayExtend((payload) =>
                     param.onPlayExtend(payload),
